f818e2d21150d04a785666e463d431bd4ffce423
Christian Williams <christian.b.williams@gmail.com>
2021-11-15 22:19:22 -0500

    Class tests

---
+++ src/test/java/ucf/assignments/AppTest.java
@@ -0,0 +1,24 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void testMain() {
+    }
+
+    @Test
+    void testStart() {
+    }
+}
---
+++ src/test/java/ucf/assignments/ItemTest.java
@@ -0,0 +1,32 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getDueDate() {
+    }
+
+    @Test
+    void setDueDate() {
+    }
+
+    @Test
+    void getCompletion() {
+    }
+
+    @Test
+    void setCompletion() {
+    }
+}
